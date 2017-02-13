cc18970e8a0d82014bc2cc1ac148802e096f934b
ViolaMa <704264449@qq.com>
2017-02-13 15:12:36 +0800

    百度新闻手机客户端仿

--- js/news.js
+++ js/news.js
@@ -2,26 +2,37 @@
  * Created by admin on 2017/1/18.
  */
 $(function () {
-    refreshNews();
+    refreshNews('推荐');
+
+    $("nav a").click(function(e){
+        e.preventDefault();
+        var type = $(this).text();
+        refreshNews(type);
+    })
 })
 
-function refreshNews(){
+function refreshNews(type) {
     var _lists = $("article ul.news-lists");
     _lists.empty();
     $.ajax({
-        url:'./server/getnews.php',
-        type:'get',
-        dataType:'json',
-        success:function(data){
-            console.log(data);
-            var _list = $("<li></li>").addClass("news-list").prependTo(_lists);
-            var _newImg = $("<div></div>").addClass("newsimg").appendTo(_list);
-            var _img = $("<img>").attr("src",data.newsimg).appendTo(_newImg);
-            var _newContent = $("<div></div>").addClass("newscontent").appendTo(_list);
-            var _h1 = $("<h1></h1>").html(data.newstitle).appendTo(_newContent);
-            var _p = $("<p></p>").appendTo(_newContent);
-            var _newstime = $("<span></span>").addClass("newstime").html(data.newstime).appendTo(_p);
-            var _newssrc = $("<span></span>").addClass("newssrc").html(data.newssrc).appendTo(_p);
+        url: './server/getnews.php',
+        type: 'get',
+        datatype: 'json',
+        data:{ newstype:type},
+        success: function (data) {
+            data.forEach(function (item,index,array) {
+                var _list = $("<li></li>").addClass("news-list").prependTo(_lists);
+                var _newImg = $("<div></div>").addClass("newsimg").appendTo(_list);
+                var _img = $("<img>").attr("src",item.newsimg).appendTo(_newImg);
+                var _newContent = $("<div></div>").addClass("newscontent").appendTo(_list);
+                var _h1 = $("<h1></h1>").html(item.newstitle).appendTo(_newContent);
+                var _p = $("<p></p>");
+                var _newstime = $("<span></span>").addClass("newstime").html(item.addtime);
+                var _newssrc = $("<span></span>").addClass("newssrc").html(item.newssrc);
+                _p.append(_h1,_newstime,_newssrc);
+                _newContent.append(_p);
+            })
+
         }
     })
 
